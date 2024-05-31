1561ccd208428334e683b6238b7855d7ebb7cd38
Jeff Barlow <jeff.barlow.spady@gmail.com>
2024-05-31 11:59:54 -0600

    add TextNode class and unit tests

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from textnode import TextNode
+
+
+#print('Hello World')
+
+def main():
+    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(node)
+    repr(node)
+
+
+
+main()
+    
